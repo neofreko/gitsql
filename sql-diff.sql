65bbca590bbb1f8bea989ecac715ef231c5d9928
neofreko <akhmadf@gmail.com>
2012-05-23 21:47:14 +0900

    sql-diff

--- sql-diff.sql
+++ sql-diff.sql
@@ -1,9 +1,9 @@
 ## mysqldiff 0.43
 ## 
-## Run on Wed May 23 21:45:15 2012
+## Run on Wed May 23 21:47:12 2012
 ## Options: user=root, debug=0, host=localhost
 ##
 ## ---   db: test2 (host=localhost user=root)
 ## +++   db: test (host=localhost user=root)
 
-ALTER TABLE new_table ADD COLUMN b varchar(45) DEFAULT NULL;
+ALTER TABLE new_table ADD INDEX idx_b (b);
