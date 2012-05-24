0fa7e8dee63d0ddb0e2dc82af21486386b454dec
neofreko <akhmadf@gmail.com>
2012-05-24 12:57:55 +0900

    succesfully generate diff, apply to test db and commit to git

--- commitsql.py
+++ commitsql.py
@@ -2,6 +2,7 @@
 import cli.app
 import commands
 import time
+from time import gmtime, strftime
 
 @cli.app.CommandLineApp
 def commitsql(app):
@@ -13,7 +14,11 @@ def commitsql(app):
 	min_p = ''
 	if (app.params.password):
 		min_p = '-p'
-	print commands.getoutput('%s -u %s -h localhost %s %s test_%s > %s' % (app.params.mysqldiff, app.params.user, min_p, app.params.dbname, app.params.dbname, tmp_file))
+
+	print commands.getoutput('%s -u %s -h localhost %s test_%s %s > %s' % (app.params.mysqldiff, app.params.user, min_p, app.params.dbname, app.params.dbname, tmp_file))
+
+	print "sql diff:"
+	print commands.getoutput('cat %s' % tmp_file)
 
 	# apply to db
 	successful = commands.getstatusoutput('%s -u %s -h localhost %s test_%s < %s' % (app.params.mysqlpath, app.params.user, min_p, app.params.dbname, tmp_file))
@@ -24,14 +29,14 @@ def commitsql(app):
 
 	# commit sql if successful
 	print "sql diff applied to test_db. Committing changes to git.."
-	#commands.getoutput('cat %s > sql-diff.sql' % tmp_file)
-	#commands.getoutput('git commit sql-diff -m "%s"' % "sql diff")
+	commands.getoutput('cat %s > sql-diff.sql' % tmp_file)
+	print commands.getoutput('git commit sql-diff.sql -m "sql diff %s"' % strftime("%a, %d %b %Y %H:%M:%S +0000", gmtime()))
 
-preparedb.add_param("-d", "--dbname", help="prepare a comparison db for internal sql diffing in each commit", default="test_test", action="store")
-preparedb.add_param("-m", "--mysqldiff", help="mysqldiff executable path", default="mysqldiff", action="store")
-preparedb.add_param("-u", "--user", help="mysql user", default="root", action="store")
-preparedb.add_param("-p", "--password", help="enable ask password", default=False, action="store_true")
-preparedb.add_param("-x", "--mysqlpath", help="mysql executable path", default="/Applications/XAMPP/xamppfiles/bin/mysql", action="store")
+commitsql.add_param("-d", "--dbname", help="prepare a comparison db for internal sql diffing in each commit", default="test", action="store")
+commitsql.add_param("-m", "--mysqldiff", help="mysqldiff executable path", default="mysqldiff", action="store")
+commitsql.add_param("-u", "--user", help="mysql user", default="root", action="store")
+commitsql.add_param("-p", "--password", help="enable ask password", default=False, action="store_true")
+commitsql.add_param("-x", "--mysqlpath", help="mysql executable path", default="/Applications/XAMPP/xamppfiles/bin/mysql", action="store")
 
 
 if __name__ == "__main__":
